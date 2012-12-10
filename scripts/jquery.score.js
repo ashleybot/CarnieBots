c48ddb8d34886797fdb82bdd196f1bf2457cf001
ashleygreer <e.a.greer@gmail.com>
2012-12-09 22:37:14 -0500

    really works this time

--- scripts/jquery.score.js
+++ scripts/jquery.score.js
@@ -5,78 +5,109 @@
 // python -m SimpleHTTPServer 4101
 // then navigate to 0.0.0.0:4101 in your favorite web browser
 
-var level;
-var key;
 $(document).ready(function(){
   // find out which level we are on, EASY or HARD
-  setLevel();
+  currentLevel();
   getScores(); // initializes scores if none exist
   initializeModules();
 
 });
 
-// we have two levels for the control panel
-function setLevel(){
-  var storedLevel = document.cookie["CarnieBotScoreKey"];
-  if (storedLevel == null) {
-    level = "easy";
-    document.cookie = "CarnieBotScoreKey=easy"; // this is for the case when the control panel hasn't been accessed
-  }
-  if (document.URL.indexOf("easy") >= 0){
+// which level are we on? Easy or hard?
+// gets and sets (if necessary)
+function currentLevel() {
+  var level = $.cookie("CarnieBotBalloonLevel");
+  // if the cookie hasn't been set it is the first time through and therefore easy
+  if (level == null) {
     level = "easy";
   }
-  else if (document.URL.indexOf("hard") >= 0){
+  // also test if the user is on the hard control panel, and then set it to upgrade
+  if (document.URL.indexOf("hard") >= 0){
     level = "hard";
-    document.cookie = "CarnieBotScoreKey=hard"; // upgrade
   }
-  key = "CarnieBotScoreEasy";
+  $.cookie("CarnieBotBalloonLevel", level);
+  return level;
+}
+
+function getScoreKey() {
+  var level = currentLevel();
+  var key = "CarnieBotScoreEasy";
   if (level == "hard"){
     key = "CarnieBotScoreHard";
   }
-  console.log(level);
+  return key;
 }
 
 // save the default state (the scores should be all mixed up here)
 // the number is the module number, not the position because position is fixed
 function getScores(){
+
+  var key = getScoreKey();
+  var level = currentLevel();
   var balloonPoints = $.JSONCookie(key);
-  
-  if (balloonPoints == null){
+  console.log("get " + key);
+  console.log(JSON.stringify(balloonPoints));
+  // default points for each level
+  if (balloonPoints == null || JSON.stringify(balloonPoints).length == 2){
     if (level == "easy"){
       balloonPoints = {"red":"two", "yellow":"one", "blue":"three"};
     }
     else{
-      balloonPoints = {"red":"two", "purple":"one", "orange":"three", "green":"four"};
+      balloonPoints = {"red":"two", "pink":"one", "orange":"three", "green":"four"};
     }
-    
+    console.log("store default");
     $.JSONCookie(key, balloonPoints, {path: '/'}); 
   }
-  console.log("getScores " + JSON.stringify(balloonPoints));
+  console.log("getScores " + level + " " + JSON.stringify(balloonPoints));
   return balloonPoints;
 }
 
 // on initial page load, arrange the modules to match what is in the cookie
 function initializeModules(){
-  var moduleArrangement = $.JSONCookie(key);
-  if (moduleArrangement != null){
-    var redModuleListItem = $("#" + moduleArrangement.red);
-    var yellowModuleListItem = $("#" + moduleArrangement.yellow);
-    var blueModuleListItem = $("#" + moduleArrangement.blue);
-    
-    // the following positioning is based on the background image
-    // if that changes then this needs to be updated
-    yellowModuleListItem.insertBefore(blueModuleListItem);
-    redModuleListItem.insertBefore(yellowModuleListItem);
+  
+  var moduleArrangement = getScores(); // the module arrangement is the scores
+  
+  if (JSON.stringify(moduleArrangement).length > 2){
+    if (JSON.stringify(moduleArrangement).indexOf("yellow") >= 3) {
+      var redModuleListItem = $("#" + moduleArrangement.red);
+      var yellowModuleListItem = $("#" + moduleArrangement.yellow);
+      var blueModuleListItem = $("#" + moduleArrangement.blue);
+      
+      // the following positioning is based on the background image
+      // if that changes then this needs to be updated
+      yellowModuleListItem.insertBefore(blueModuleListItem);
+      redModuleListItem.insertBefore(yellowModuleListItem);
+    }
+    else {
+      // this is so complicated because the red balloon is repeated in both puzzles
+      var redModule = $("#" + moduleArrangement.red);
+      var pinkModule = $("#" + moduleArrangement.pink);
+      var orangeModule = $("#" + moduleArrangement.orange);
+      var greenModule = $("#" + moduleArrangement.green);
+      
+      redModule.insertBefore(orangeModule);
+      pinkModule.insertBefore(redModule);
+      greenModule.insertBefore(pinkModule);
+    }
   }
 }
 
 function setScores(arrayOfItems, movedItem){
   var balloonPoints = getScores();
+  var key = getScoreKey();
   // iterate through array and reset cookie
-  if (arrayOfItems != null && arrayOfItems.length > 2){
-    balloonPoints.red = arrayOfItems[0];
-    balloonPoints.yellow = arrayOfItems[1];
-    balloonPoints.blue = arrayOfItems[2];
+  if (arrayOfItems != null) {
+    if (arrayOfItems.length == 3){
+      balloonPoints.red = arrayOfItems[0];
+      balloonPoints.yellow = arrayOfItems[1];
+      balloonPoints.blue = arrayOfItems[2];
+    }
+    else if (arrayOfItems.length == 4){
+      balloonPoints.green = arrayOfItems[0];
+      balloonPoints.pink = arrayOfItems[1];
+      balloonPoints.red = arrayOfItems[2];  
+      balloonPoints.orange = arrayOfItems[3];  
+    }
   }
   $.JSONCookie(key, balloonPoints, {path: '/'}); // key is global
 }
@@ -84,7 +115,7 @@ function setScores(arrayOfItems, movedItem){
 function addScore(balloonState, color)
 {
   var balloonPoints = getScores();
-  console.log(JSON.stringify(balloonPoints));
+  console.log("addscore " + JSON.stringify(balloonPoints));
   if (balloonState == 0){
     var point = getInteger(balloonPoints[color]);
     console.log("point " + point);
