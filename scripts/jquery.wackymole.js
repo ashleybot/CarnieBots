af6d8caf8e8e27e60446ad3c24c164371812b796
Joshua Wilson <jbw04@live.com>
2012-12-09 19:37:29 -0500

    Fixed Mole Movement

--- scripts/jquery.wackymole.js
+++ scripts/jquery.wackymole.js
@@ -1,10 +1,14 @@
 $(document).ready(function () {
-
-  var molePositions = [0, 1, 2, 3, 4];
-  var currentMolePositionIndex = 0;
-  var molePosition;
+  $('#whackButton').click(function (){
+    whack();
+  });
   moveMole();
 });
+
+var molePosition;
+var molePositions = [2, 1, 2, 3, 4];
+
+var currentMolePositionIndex = 0;
 
 function moveMole(){
   molePosition = molePositions[currentMolePositionIndex];
@@ -20,6 +24,19 @@ function moveMole(){
 }
 
 function animateMole(moleNumber) {
-  var moleToAnimate = "#mole" + moleNumber;
-  $(moleToAnimate).toggleClass('topRowMoleOut bottomRowMoleOut', 1000).toggleClass('topRowMoleIn bottomRowMoleIn', 1000);
+  var moleToAnimate = $("#mole" + (moleNumber + 1));
+  if (moleToAnimate.hasClass('topRowMoleOut') || moleToAnimate.hasClass('topRowMoleIn')) {
+    moleToAnimate.toggleClass('topRowMoleOut', 1000).toggleClass('topRowMoleIn', 1000);
+  }
+  else{
+    moleToAnimate.toggleClass('bottomRowMoleOut', 1000).toggleClass('bottomRowMoleIn', 1000);
+  }  
+}
+
+function whack() {
+  var whackyGuess = ($('#whackyGuess').val() );
+  alert(whackyGuess);
+  if (whackyGuess == molePosition) {
+    moveMole();
+  }
 }
