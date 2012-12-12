523de655806bc82a6d9421001a214982b6d90aa6
Luna <miss.luna.ruan@gmail.com>
2012-12-12 18:57:16 -0500

    Wackamole -> animations/scores still don't work.

--- scripts/jquery.wackymole.js
+++ scripts/jquery.wackymole.js
@@ -1,16 +1,42 @@
 // setup; make some variables available to the full scope and set up the button.
 $(document).ready(function () {
-  $('#whackButton').click(function (){
-    whack();
-  });
-  moveMole();
+	$('body').keypress(function(event) {
+		console.log("pressed");
+		if (isReset === false) return;
+		else{
+			isReset = false;
+			switch(event.which){
+				case 48:
+					whack(0);
+					break;
+				case 49:
+					whack(1);
+					break;
+				case 50:
+					whack(2);
+					break;
+				case 51:
+					whack(3);
+					break;
+				case 52:
+					whack(4);
+					break;
+				default:
+					alert("Ha ha! You missed! Why don't you try picking a number from 0 to 4?");
+					animateMole(molePosition);
+					setTimeout(reset, 1500);
+					break;
+			}
+		}
+	});
+	moveMole();
 });
 
 var molePosition;
 var molePositions = [2, 1, 0, 3, 4];
-
+var isReset = true;
 var currentMolePositionIndex = 0;
-
+var moleCorrect = false;
 // Choose a new mole position; uses the array 'molePositions' and goes in sequence.  After the last index in the
 // array, this wraps around to the first element in the array and starts over
 function moveMole(){
@@ -32,18 +58,53 @@ function moveMole(){
 
 // animates the specified mole (mole numbers are 1-indexed!)
 function animateMole(moleNumber) {
-  var moleToAnimate = $("#mole" + (moleNumber + 1));
-  if (moleToAnimate.hasClass('topRowMoleOut') || moleToAnimate.hasClass('topRowMoleIn')) {
+	var moleToAnimate = $("#mole" + (moleNumber + 1));
+	if (moleToAnimate.hasClass('topRowMoleIn')){
+	    moleToAnimate.toggleClass('topRowMoleOut', 1000).toggleClass('topRowMoleIn', 1000);
+	}
+	else if (moleToAnimate.hasClass('bottomRowMoleIn')){
+		moleToAnimate.toggleClass('bottomRowMoleOut', 1000).toggleClass('bottomRowMoleIn', 1000);
+	}
+	else{
+		var i = 0, max = 2;
+		var slowloop = function (){
+			if (i < max){
+				changeState(moleToAnimate, i+1);
+				setTimeout(slowloop, 100);
+				i++;
+			}
+			else{
+				var byeByeMole = function(){
+					if(moleToAnimate.hasClass('topRowMoleOut')){
+						moleToAnimate.toggleClass('topRowMoleOut', 1000).toggleClass('topRowMoleIn', 1000);
+					}
+					else{
+						moleToAnimate.toggleClass('bottomRowMoleOut', 1000).toggleClass('bottomRowMoleIn', 1000);
+					}
+					moleToAnimate.attr('src', "images/wackyMole/mole.png");
+				}
+				setTimeout(byeByeMole, 400);
+			}
+		};
+		slowloop();
+	}
+
+/*  if (moleToAnimate.hasClass('topRowMoleOut') || moleToAnimate.hasClass('topRowMoleIn')) {
     moleToAnimate.toggleClass('topRowMoleOut', 1000).toggleClass('topRowMoleIn', 1000);
   }
   else{
     moleToAnimate.toggleClass('bottomRowMoleOut', 1000).toggleClass('bottomRowMoleIn', 1000);
-  }  
+  }  */
 }
 
 // animates the mallet and dispatches actions for whether or not the player's entry was correct
-function whack() {
-  var whackyGuess = ($('#whackyGuess').val());
+
+function changeState(mole, index){
+    mole.attr('src', "images/wackyMole/mole" + index + ".png");
+}
+
+function whack(whackyGuess) {
+ // var whackyGuess = ($('#whackyGuess').val());
 
   if (isEven(whackyGuess)) {
     $('#mallet').addClass('topRowWhack');
@@ -59,6 +120,10 @@ function whack() {
   if (whackyGuess == molePosition) {
     //do something with score here
   }
+  else{
+	alert("Ha ha! You missed! You chose hole number "+whackyGuess+  
+			", but I'm not in that hole!")
+  }
   animateMole(molePosition);
 
 
@@ -70,6 +135,7 @@ function whack() {
 function reset() {
   $('#mallet').removeClass().addClass('mallet gamePiece');
   moveMole();
+  isReset = true;
 }
 
 // determines if specified number is even
