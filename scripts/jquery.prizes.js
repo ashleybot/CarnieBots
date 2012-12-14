252460430789639124b6e92c36ad7c36225ca5cc
ashleygreer <e.a.greer@gmail.com>
2012-12-14 11:19:57 -0500

    working on prizes

--- scripts/jquery.prizes.js
+++ scripts/jquery.prizes.js
@@ -5,7 +5,7 @@ function canGetPrizes(level){
   var count = 0;
   if ("easyBalloon"){
     count = parseInt($.cookie("EasyBalloonPrizes"));
-    if (count < 4){
+    if (count < 3){
       count++;
       $.cookie("EasyBalloonPrizes", count, {path: '/game'});
       return true;
@@ -13,7 +13,7 @@ function canGetPrizes(level){
   }
   else if ("hardBalloon"){
     count = parseInt($.cookie("HardBalloonPrizes"));
-    if (count < 4){
+    if (count < 3){
       count++;
       $.cookie("HardBalloonPrizes", count, {path: '/game'});
       return true;
@@ -23,11 +23,40 @@ function canGetPrizes(level){
   return false;
 }
 
+function displayPrizes() {
+  // 300, 80
+  // 500, 200
+  // 100, 450
+  // 400, 600
+  // 600, 500
+  // 280, 870
+  
+  var currentlyStoredPrizes = $.JSONCookie("CurrentlyStoredPrizes");
+  if (currentlyStoredPrizes.zero.length > 0){
+    
+  }
+}
+
 function storePrize(filename,index){
   // convert index to x,y
+  if ((index > -1) && (index < 6)){
+    console.log("Storing prize");
+  }
+  else{
+    //show a message that it can't be stored
+  }
+  
+  
+  var storedPrizes = {"zero":"", "one":"", "two":"", "three":"", "four":"", "five":""};
+  $.JSONCookie("CurrentlyStoredPrizes", storedPrizes, {path: '/game'}); 
 }
 
 function getPrizeFromBoard(index){
+}
+
+function storeCurrentPrize(index){
+  var currentPrize = getCurrentPrize();
+  storePrize(currentPrize, index);
 }
 
 function getCurrentPrize() {
@@ -38,9 +67,8 @@ function getCurrentPrize() {
   var count = parseInt(prizesCollected);
   
   if (count > -1) {
-    fileName = prizesCollected[count];
+    fileName = prizeFileNames[count];
   }
-  
   return fileName;
 }
 
@@ -66,4 +94,7 @@ function clearPrizes() {
   $.cookie("PrizesCollected", "-1", {path: '/game'});
   $.cookie("EasyBalloonPrizes", "0", {path: '/game'});
   $.cookie("HardBalloonPrizes", "0", {path: '/game'});
+  
+  var storedPrizes = {"zero":"", "one":"", "two":"", "three":"", "four":"", "five":""};
+  $.JSONCookie("CurrentlyStoredPrizes", storedPrizes, {path: '/game'});
 }
